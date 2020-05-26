dfce5760c9386d2310ff1f0826da7e2078ae0144
Archi-R <kkromans009@gmail.com>
2020-05-25 20:16:16 -0400

    sections complete

--- js/app.js
+++ js/app.js
@@ -10,15 +10,17 @@ img_3.style.width = `calc((${img_1.offsetWidth}px + ${img_2.offsetWidth}px) / 2)
 img_3.style.left = `calc(50% - (${img_3.offsetWidth}px / 2))`;
 header.style.height = `calc(100vh - ${nav.offsetHeight}px*5)`;
 window.onload = (event) => {
-  circle.style.display = 'inherit'
+  circle.style.display = 'inherit';
+  
 };
 
 window.addEventListener('resize', () => { 
 	circle.style.top = `calc(${header.offsetHeight}px - 25px)`
+	img_3.style.top = `calc(${container_1.offsetTop}px)`;
 });
 
 circle.style.top = `calc(${header.offsetHeight}px - 25px)`;
-img_3.style.top = `calc(${container_1.offsetTop}px + 7.5em)`;
+img_3.style.top = `calc(${container_1.offsetTop}px + 7rem)`;
 
 // circle.addEventListener('click', () => {
 // 	circle.getElementsByTagName('svg')[0].style.transform = 'rotate(180deg)'
